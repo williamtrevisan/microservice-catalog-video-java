3914b118d7ee917097b6f229ac5e412130a741c8
William Trevisan <williamtrev@hotmail.com>
2022-07-05 13:42:09 -0300

    feat: create no stacktrace exception

--- domain/src/main/java/com/trevisan/catalog/video/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/trevisan/catalog/video/domain/exceptions/DomainException.java
@@ -4,16 +4,20 @@ import com.trevisan.catalog.video.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException {
+public class DomainException extends NoStacktraceException {
     private final List<Error> errors;
 
-    protected DomainException(final List<Error> anErrors) {
-        super("", null, true, false);
+    protected DomainException(final String aMessage, final List<Error> anErrors) {
+        super(aMessage);
         this.errors = anErrors;
     }
 
+    public static DomainException with(final Error anError) {
+        return new DomainException(anError.message(), List.of(anError));
+    }
+
     public static DomainException with(final List<Error> anErrors) {
-        return new DomainException(anErrors);
+        return new DomainException("", anErrors);
     }
 
     public List<Error> getErrors() {
