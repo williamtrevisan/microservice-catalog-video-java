c4084a8e8970310f8cf74551f5d5f0bc02292118
William Trevisan <williamtrev@hotmail.com>
2022-07-05 14:50:31 -0300

    feat: create category gateway

---
+++ domain/src/main/java/com/trevisan/catalog/video/domain/category/CategorySearchQuery.java
@@ -0,0 +1,10 @@
+package com.trevisan.catalog.video.domain.category;
+
+public record CategorySearchQuery(
+        int page,
+        int perPage,
+        String terms,
+        String sort,
+        String direction
+) {
+}
