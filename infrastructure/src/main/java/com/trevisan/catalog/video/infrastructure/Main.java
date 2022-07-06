1a1d6e0da9c609a8e54bc1646f6ccc461b5ce801
William Trevisan <williamtrev@hotmail.com>
2022-07-06 13:49:13 -0300

    chore: add spring-boot

--- infrastructure/src/main/java/com/trevisan/catalog/video/infrastructure/Main.java
+++ infrastructure/src/main/java/com/trevisan/catalog/video/infrastructure/Main.java
@@ -1,10 +1,15 @@
 package com.trevisan.catalog.video.infrastructure;
 
-import com.trevisan.catalog.video.application.UseCase;
+import com.trevisan.catalog.video.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(new UseCase().execute());
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infrastructure/src/main/java/com/trevisan/catalog/video/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.trevisan.catalog.video.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.trevisan.catalog.video")
+public class WebServerConfig {
+}
